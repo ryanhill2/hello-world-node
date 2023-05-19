55aa219c3ad5adc79b8b896abf0b1668723a915e
ryanhill <ryanhill00101@gmail.com>
2023-05-19 23:59:50 +0100

    Add docker file to create docker container for this app

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res) => {
+app.get('/ryan', (req, res) => {
     res.send('Something else');
 });
 
